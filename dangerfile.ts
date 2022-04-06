fc9941c604aab4d0ce469f8dc0b3550efb9f45e2
Xavier MALPARTY <xavier@nimblehq.co>
2022-04-06 11:20:50 +0700

    Test removing danger sample code

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,4 @@
-import { message, danger } from 'danger';
 import { codeCoverage, PluginOptions } from 'danger-plugin-code-coverage';
-
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
 
 const defaultPluginOptions: PluginOptions[] = [
   {
