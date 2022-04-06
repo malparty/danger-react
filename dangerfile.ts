9e995c5471382d78d0f4a8b659a44da3a967fd44
Xavier MALPARTY <xavier@nimblehq.co>
2022-04-06 12:26:49 +0700

    Add both

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,3 +1,5 @@
+import { schedule } from 'danger';
+import { istanbulCoverage } from 'danger-plugin-istanbul-coverage';
 import { codeCoverage, PluginOptions } from 'danger-plugin-code-coverage';
 
 const defaultPluginOptions: PluginOptions[] = [
@@ -11,44 +13,41 @@ const defaultPluginOptions: PluginOptions[] = [
 
 codeCoverage(defaultPluginOptions);
 
-// import { schedule } from 'danger';
-// import { istanbulCoverage } from 'danger-plugin-istanbul-coverage';
+schedule(istanbulCoverage()); // Use default configuration
 
-// schedule(istanbulCoverage()); // Use default configuration
+schedule(
+  istanbulCoverage({
+    // Set a custom success message
+    customSuccessMessage: 'Congrats, coverage is good',
 
-// schedule(
-//   istanbulCoverage({
-//     // Set a custom success message
-//     customSuccessMessage: 'Congrats, coverage is good',
+    // Set a custom failure message
+    customFailureMessage: 'Coverage is a little low, take a look',
 
-//     // Set a custom failure message
-//     customFailureMessage: 'Coverage is a little low, take a look',
+    // How to sort the entries in the table
+    entrySortMethod: 'alphabetically', // || "least-coverage" || "most-coverage" || "largest-file-size" ||"smallest-file-size" || "uncovered-lines"
 
-//     // How to sort the entries in the table
-//     entrySortMethod: 'alphabetically', // || "least-coverage" || "most-coverage" || "largest-file-size" ||"smallest-file-size" || "uncovered-lines"
+    // Add a maximum number of entries to display
+    numberOfEntries: 10,
 
-//     // Add a maximum number of entries to display
-//     numberOfEntries: 10,
+    // The location of the istanbul coverage file.
+    // coveragePath: '.nyc_output/out.json', // The merged JSON coverage data
+    // Alternatively, if you have multiple coverage summaries, you can merge them into one report
+    // coveragePaths: ["./dir1/coverage-summary.json", "./dir2/coverage-summary.json"]
+    // You can also specify the format, instead of letting it be inferred from the file name
+    coveragePath: { path: './coverage/merged/lcov.info', type: 'lcov' /* ||  "json-summary" */ },
 
-//     // The location of the istanbul coverage file.
-//     // coveragePath: '.nyc_output/out.json', // The merged JSON coverage data
-//     // Alternatively, if you have multiple coverage summaries, you can merge them into one report
-//     // coveragePaths: ["./dir1/coverage-summary.json", "./dir2/coverage-summary.json"]
-//     // You can also specify the format, instead of letting it be inferred from the file name
-//     coveragePath: { path: './coverage/merged/lcov.info', type: 'lcov' /* ||  "json-summary" */ },
+    // Which set of files to summarise from the coverage file.
+    reportFileSet: 'all', // || "modified" || "created" || "createdOrModified"
 
-//     // Which set of files to summarise from the coverage file.
-//     reportFileSet: 'all', // || "modified" || "created" || "createdOrModified"
+    // What to do when the PR doesn't meet the minimum code coverage threshold
+    reportMode: 'message', // || "warn" || "fail"
 
-//     // What to do when the PR doesn't meet the minimum code coverage threshold
-//     reportMode: 'message', // || "warn" || "fail"
-
-//     // Minimum coverage threshold percentages. Compared against the cumulative coverage of the reportFileSet.
-//     threshold: {
-//       statements: 100,
-//       branches: 100,
-//       functions: 100,
-//       lines: 100,
-//     },
-//   })
-// );
+    // Minimum coverage threshold percentages. Compared against the cumulative coverage of the reportFileSet.
+    threshold: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  })
+);
